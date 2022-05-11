f9188d47642747e9a75d8cd03d2442e6a4058cb3
Sanjay Murmu <sanjaymurmu40work@gmail.com>
2022-05-12 00:38:31 +0530

    removeNode method added

--- Tree/tree_addNode.js
+++ Tree/tree_addNode.js
@@ -31,6 +31,31 @@ class Node {
         }
     }
 
+    removeNode(value) {
+        const segments = value.split('/');
+
+        if (segments.length === 0) {
+            return;
+        }
+        if (segments.length == 1) {
+            const existingNodeIndex = this.children.findIndex(child => child.value === segments[0]);
+
+            if (existingNodeIndex < 0) {
+                throw new Error('Could not find the matching value!');
+            }
+            // if existingNodeIndex >= 0
+            this.children.splice(existingNodeIndex, 1); // add existingNodeIndex, remove 1 elem
+        }
+
+        if (segments.length > 1) {
+            const existingChildNode = this.children.find(child => child.value === segments[0])
+
+            if (!existingChildNode) {
+                throw new Error("Could not find matching path! path segment: " + segments[0])
+            }
+            existingChildNode.removeNode(segments.slice(1).join('/')); // slice (str,end)
+        }
+    }
 }
 
 class Tree {
@@ -42,6 +67,9 @@ class Tree {
         this.root.addNode(path);
     }
 
+    removeNode(path) {
+        this.root.removeNode(path);
+    }
 }
 
 const filesystem = new Tree('/');
@@ -49,5 +77,7 @@ filesystem.add('documents');
 filesystem.add('documents/personal/tax.docs');
 filesystem.add('games/cod.exe');
 filesystem.add('games/cod2.exe');
+
+filesystem.removeNode('games/cod.exe');
 
 console.log(filesystem);
